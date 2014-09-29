716923e4d340068ebb44515e3dab06915274f3e9
Peter Millard <24697@longroad.ac.uk>
2014-09-29 14:08:58 +0100

    python alogthums that need to be written up

---
+++ Analysis/Algothums/handicap times.py
@@ -0,0 +1,6 @@
+#algothum of calcutaing handicap times
+riders_times = [[times][riders]] #list of all the times and the rider assoscatied with it
+for count in range(len(times)):
+    handicap = look_up_tabel(riders_times[][count]best_time)
+    handicaped_times = [[][]]
+    handicaped_times.append(riders_times[[count][]]-hadicap)
